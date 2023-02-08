27f478c3dc98dda657eae5e132a46f6085fe9d7a
rayanneacb <rayannefernandacb@gmail.com>
2023-02-08 16:17:31 -0300

    Configurando token de account

--- account/src/repositories/accountRepository.js
+++ account/src/repositories/accountRepository.js
@@ -1,7 +1,19 @@
 import { User } from "../../models/user.js";
+import client from "../repositories/databaseClient.js" 
 
 export async function saveAccount(user) {
     const createdAccount = await User.create(user)
     await createdAccount.save();
     return createdAccount;
+}
+export async function findUserByEmail(email) {
+    //const usersCollection = await getUsersCollection();
+    //console.log (usersCollection)
+    const user = await User.findOne({ where: {email} });
+    return user;
+} 
+async function getUsersCollection() {
+
+    const allUsers = await User.findAll();
+    return allUsers
 }
