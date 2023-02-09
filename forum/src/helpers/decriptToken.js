f87aed061b40710654bc016970c0ab55e72b1fa3
RastelliBru <bruna.rastelli@gmail.com>
2023-02-09 17:52:46 -0300

    creating authentication token in questions and answers

---
+++ forum/src/helpers/decriptToken.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+
+function decriptToken(token) {
+    try {
+        const tokenSecret =process.env.TOKEN_SECRET;
+        const { userId } = jwt.verify(token, tokenSecret);
+
+        return { userId }; 
+
+    } catch(error){
+        console.log('error when verifying token', error.message);
+        return {};
+    }
+}
+
+module.exports = {decriptToken}
